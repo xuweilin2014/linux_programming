1804e813d8589c698b525a770458c36b8686a643
xuweilin <xuweilin920@163.com>
2022-11-14 16:34:39 +0800

    使用 exit 替换 return

--- process/zoom_test.c
+++ process/zoom_test.c
@@ -14,13 +14,13 @@ int zoom_test() {
 
     if (pid == 0) {
         printf("---child, my parent=%d, my=%d, going to sleep 10s\n", getppid(), getpid());
-        sleep(15);
         printf("-------------child die-------------\n");
-        return 73;
+        exit(73);
     } else if (pid > 0) {
         // wait(NULL); 不关心子进程终止的原因
         // 如果子进程未终止，父进程阻塞在这个函数上
         // 一次 wait/waitpid 函数调用，只能回收随机一个子进程
+        sleep(5);
         wpid = wait(&status);
         if (wpid == -1) {
             perror("wait err");
