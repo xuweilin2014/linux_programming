e9f6e427b5ed5aa2b4833ef43598ca78d2a13a4b
xuweilin <xuweilin920@163.com>
2022-11-21 00:31:29 +0800

    libevent mkfifo 命名管道

---
+++ libevent/read_fifo.c
@@ -0,0 +1,62 @@
+//
+// Created by xuweilin on 22-11-20.
+//
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <event2/event.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <event.h>
+
+#define FIFO_ADDR "my_fifo"
+
+// 写操作回调函数
+void read_cb(evutil_socket_t fd, short what, void *argc) {
+
+    // 读管道
+    char buf[1024] = {0};
+
+    int len = read(fd, buf, sizeof(buf));
+
+    printf("read event: %s \n", what & EV_READ ? "Yes" : "No");
+    printf("data len = %d, buf = %s\n", len, buf);
+
+}
+
+int main() {
+
+    unlink(FIFO_ADDR);
+    // 创建有名管道
+    mkfifo(FIFO_ADDR, 0664);
+
+    int fd = open(FIFO_ADDR, O_RDONLY);
+    if (fd == -1) {
+        perror("open error");
+        exit(1);
+    }
+
+    // 创建一个 event_base
+    struct event_base *base = NULL;
+    // 创建事件
+    struct event* ev;
+
+    base = event_base_new();
+    // 监听读事件，同时事件一直持续
+    ev = event_new(base, fd, EV_READ | EV_PERSIST, read_cb, base);
+
+    // 添加事件
+    event_add(ev, NULL);
+    // 开启事件循环
+    event_base_dispatch(base);
+
+    // 释放资源
+    event_free(ev);
+    event_base_free(base);
+
+    close(fd);
+
+    return 0;
+}
